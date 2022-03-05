899966600dab170579a28a32f6173e72253e5626
Chlesea <1556792948@qq.com>
2022-03-05 20:00:24 +0800

    王道第三周第十一天作业

---
+++ WDWork/w4d11.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Administrator on 2022/3/5.
+//
+
+# include "iostream"
+# include "string"
+
+int main() {
+    using namespace std;
+    int n;
+    cin >> n ;
+    cin.get();
+    string s1;
+    getline(cin, s1);
+    cout << s1;
+
+}
