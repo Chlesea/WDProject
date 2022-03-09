b008baafd812f1ee27fc71d7ea89c44acd180698
Chlesea <1556792948@qq.com>
2022-03-09 19:19:23 +0800

    中级第二天

---
+++ WDWork_Middle/d2.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Administrator on 2022/3/8.
+//
+
+#include "iostream"
+#include "string"
+
+int main() {
+    using namespace std;
+    string s = "";
+    getline(cin,s);
+    cout << s;
+}
